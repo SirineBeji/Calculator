81c1ba0435ce1b6f9b268873f5fedde15ca77777
Sirine Beji <sirine.beji.1994@gmail.com>
2018-03-04 16:27:12 +0100

    first

---
+++ src/main/java/Calcuator/Calcuator/App.java
@@ -0,0 +1,14 @@
+package Calcuator.Calcuator;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+    	Calculator calculator = new Calculator();
+        System.out.println( calculator.add(120, 150));
+    }
+}
---
+++ src/test/java/Calcuator/Calcuator/CalculatorTest.java
@@ -0,0 +1,33 @@
+package Calcuator.Calcuator;
+
+import org.junit.Test;
+
+import junit.framework.TestCase;
+
+public class CalculatorTest extends TestCase {
+	
+	Calculator calculator;
+	
+	public CalculatorTest(){
+		super();
+		this.calculator = new Calculator();
+	}
+	
+	@Test
+	public void testAdd(){
+		assertEquals(10, this.calculator.add(5,5));
+		assertEquals(5, this.calculator.add(2, 3));
+	}
+	
+	@Test
+	public void testMultiply(){
+		assertEquals(25, this.calculator.multiply(5,5));
+		assertEquals(50, this.calculator.multiply(5,10));
+	}
+
+	@Test
+	public void testMultiplyByZero(){
+		assertEquals(5, this.calculator.multiply(5,0));
+	}
+	
+}
